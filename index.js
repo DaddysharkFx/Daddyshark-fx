79893cda69c70306f4479f249ee98af3a203ee15
DaddysharkFx <ijahman.dido@gmail.com>
2026-03-20 01:14:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,48 @@ function connect(token) {
         }));
     };
 
-    ws.onmessage = (msg) => {
-        let data = JSON.parse(msg.data);
-        console.log(data);
-    };
+    let lastDigit = null;
+
+ws.onmessage = (msg) => {
+    let data = JSON.parse(msg.data);
+
+    // After authorization, subscribe to ticks
+    if (data.msg_type === "authorize") {
+        ws.send(JSON.stringify({
+            ticks: "R_100"
+        }));
+    }
+
+    // Receive ticks
+    if (data.msg_type === "tick") {
+        let price = data.tick.quote.toString();
+        let digit = price[price.length - 1];
+
+        console.log("Last digit:", digit);
+
+        // Simple strategy: if digit is high, predict low
+        if (digit > 7) {
+            buy("DIGITUNDER", 5);
+        }
+    }
+};
+
+function buy(type, barrier) {
+    ws.send(JSON.stringify({
+        buy: 1,
+        price: 1,
+        parameters: {
+            amount: 1,
+            basis: "stake",
+            contract_type: type,
+            currency: "USD",
+            duration: 1,
+            duration_unit: "t",
+            symbol: "R_100",
+            barrier: barrier
+        }
+    }));
+}
 }
 
 app.post("/connect", (req, res) => {
